1a19f4c671de3b6f003684eb139b697ff4f10496
Jason Hughes <jason.j.hughes@vanderbilt.edu>
2025-05-30 17:13:14 -0500

    update info

--- src/wrappers/CATnWrapper.m
+++ src/wrappers/CATnWrapper.m
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 function E = CATnWrapper(A,n)
     E = cat(n,A{:});
-=======
-function E = CATnWrapper(A,n)
-    E = cat(n,A{:});
->>>>>>> 08410c48414cbfd1141b5d6a99035e1f365fbe06
-=======
-function E = CATnWrapper(A,n)
-    E = cat(n,A{:});
->>>>>>> 08410c48414cbfd1141b5d6a99035e1f365fbe06
 end
